9ee5c64a93eb2ac53c7c1372130044a3f8c7ed46
Thomas Gilon <tgi@climact.com>
2024-01-24 14:39:02 +0100

    Redefine balancing in graphes

--- scripts/graph_extraction.py
+++ scripts/graph_extraction.py
@@ -103,8 +103,7 @@ FF_ELEC = ["OCGT", "CCGT", "coal/lignite"]
 FF_HEAT = ["residential / services oil boiler", "residential / services gas boiler"]
 PRODUCTION = FF_ELEC + ["PHS", "hydro", "nuclear", "urban central biomass CHP", "solid biomass"] + RES
 H2 = ["H2 Electrolysis", "H2 Fuel Cell"]
-BALANCE = H2 + ["battery charger", "home battery charger", "BEV charger", "Haber-Bosch", "Sabatier",
-                "ammonia cracker", "helmeth", "SMR", "SMR CC", "Fischer-Tropsch"]
+BALANCE = H2 + ["battery discharger", "home battery discharger", "V2G", "ammonia cracker", "H2 Fuel Cell"]
 
 CLIP_VALUE_TWH = 1e-1  # TWh
 CLIP_VALUE_GW = 1e-3  # GW
