b5ed3d4994e85f72b471fb9ee7d1be9445592c99
VLA <vla@climact.com>
2024-02-27 14:33:08 +0100

    Add imports_exports function in streamlit

--- app/pages/3Imports_&_Exports.py
+++ app/pages/3Imports_&_Exports.py
@@ -1,8 +1,43 @@
-import streamlit as st
+from pathlib import Path
 
-from st_common import st_page_config, st_side_bar
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from st_common import network_path
+from st_common import scenario_dict
+from st_common import st_page_config
+from st_common import st_side_bar
+from scripts.graph_extraction_utils import query_imp_exp
 
 st_page_config(layout="wide")
 scenario = st_side_bar()
 
-st.title("Under construction")
+st.title("Imports and exports per carrier")
+
+df = (
+    pd.read_excel(
+        Path(network_path,
+             scenario_dict[scenario]["path"],
+             "graph_extraction_st_raw.xlsx"),
+        sheet_name="imports_exports",
+        header=0
+    )
+)
+
+country = st.selectbox('Choose your country:', list(df["node"].unique()))
+carrier = st.selectbox('Choose your carrier:', df['carrier'].unique())
+year = st.selectbox('Choose your year:', df["year"].unique())
+imports_exports = st.selectbox('Choose if imports or exports',['imports','exports'])
+  
+df = query_imp_exp(df, carrier, country, year, imports_exports)
+st.plotly_chart(
+    px.bar(
+        df,
+        title=f"{imports_exports.capitalize()} for {country} for {carrier} [TWh]",
+        barmode="group",
+        text_auto=".2s"
+    )
+    , use_container_width=True
+)
+
+st.table(df)
