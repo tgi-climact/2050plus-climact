ef4b64a514a64c06029956c1180d4396abc182f9
Thomas Gilon <tgi@climact.com>
2024-03-04 17:16:42 +0100

    Fix import / export reference

--- app/pages/3Imports_&_Exports.py
+++ app/pages/3Imports_&_Exports.py
@@ -7,12 +7,12 @@ from st_common import network_path
 from st_common import scenario_dict
 from st_common import st_page_config
 from st_common import st_side_bar
-from scripts.graph_extraction_utils import query_imp_exp
 
 st_page_config(layout="wide")
 scenario = st_side_bar()
 
 st.title("Imports and exports per carrier")
+
 
 @st.cache_data
 def get_data():
@@ -27,15 +27,31 @@ def get_data():
     )
     return df
 
+
 df = get_data()
+
+
+def query_imp_exp(df, carriers, country, year, imports_exports):
+    df_imp_exp = (
+        df.query(""
+                 "carriers == @carriers & "
+                 "year == @year & "
+                 "imports_exports == @imports_exports"
+                 )
+        .drop(["carriers", "year", "imports_exports"], axis=1)
+        .set_index('countries')
+        [country]
+    )
+    return df_imp_exp
+
 
 country = st.selectbox('Choose your country:', df["countries"].unique())
 carrier = st.selectbox('Choose your carrier:', df['carriers'].unique())
 year = st.selectbox('Choose your year:', df["year"].unique())
 imports_exports = st.selectbox('Choose if imports or exports', ['imports', 'exports'])
 df_imp_exp = (
-    pd.concat([query_imp_exp(df, carrier, [country], year, 'imports'),
-               query_imp_exp(df, carrier, [country], year, 'exports')],
+    pd.concat([query_imp_exp(df, carrier, country, year, 'imports'),
+               query_imp_exp(df, carrier, country, year, 'exports')],
               axis=1, keys=['imports', 'exports'])
 )
 df_imp_exp = (
