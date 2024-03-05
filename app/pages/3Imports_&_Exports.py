be0812bddc50ab186c059c00f8b9ca726b822766
Thomas Gilon <tgi@climact.com>
2024-03-05 09:26:52 +0100

    Improve import and exports

--- app/pages/3Imports_&_Exports.py
+++ app/pages/3Imports_&_Exports.py
@@ -48,28 +48,21 @@ def query_imp_exp(df, carriers, country, year, imports_exports):
 country = st.selectbox('Choose your country:', df["countries"].unique())
 carrier = st.selectbox('Choose your carrier:', df['carriers'].unique())
 year = st.selectbox('Choose your year:', df["year"].unique())
-imports_exports = st.selectbox('Choose if imports or exports', ['imports', 'exports'])
 df_imp_exp = (
     pd.concat([query_imp_exp(df, carrier, country, year, 'imports'),
-               query_imp_exp(df, carrier, country, year, 'exports')],
+               -1 * query_imp_exp(df, carrier, country, year, 'exports')],
               axis=1, keys=['imports', 'exports'])
 )
-df_imp_exp = (
-    df_imp_exp.drop(df_imp_exp.query('imports <= 0 and exports <=0')
-                    .index)
-    .style
-    .format(precision=2, thousands=",", decimal='.')
-)
 
-df = query_imp_exp(df, carrier, [country], year, imports_exports)
 st.plotly_chart(
     px.bar(
-        df,
-        title=f"{imports_exports.capitalize()} for {country} for {carrier} [TWh]",
-        barmode="group",
+        df_imp_exp,
+        title=f"Imports / Exports for {country} for {carrier} [TWh]",
         text_auto=".2s"
     )
     , use_container_width=True
 )
 
-st.table(df_imp_exp)
+df_imp_exp_ = df_imp_exp.drop(df_imp_exp.query('imports == 0 and exports ==0').index)
+
+st.table(df_imp_exp_.style.format(precision=2, thousands=",", decimal='.'))
